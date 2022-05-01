6a4b146f65d66443ad00368896eb6169848b50a6
ahanafislam <ahanafislam@gmail.com>
2022-05-01 06:54:55 +0600

    Create basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-success'>Hello</h1>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar collapseOnSelect expand="lg" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">Ashbab</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">Blog</Nav.Link>
+                            <Nav.Link href="#pricing">Manage Items</Nav.Link>
+                            <Nav.Link href="#pricing">Add Items</Nav.Link>
+                            <Nav.Link href="#pricing">My Item</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
