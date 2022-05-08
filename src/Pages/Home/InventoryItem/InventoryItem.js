051c209988c64236541824299821e708f942a3d6
ahanafislam <ahanafislam@gmail.com>
2022-05-08 18:02:51 +0600

    Add data in inventory items

--- src/Pages/Home/InventoryItem/InventoryItem.js
+++ src/Pages/Home/InventoryItem/InventoryItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 
-const InventoryItem = () => {
+const InventoryItem = ({item}) => {
+    const {name, img, description, price, quantity, supplier} = item;
+
     return (
         <Col>
             <Card className='service-container border-0 shadow-sm'>
-                <Card.Img variant="top" src="https://i.postimg.cc/x1nzL54b/Luzon-319.jpg" />
+                <Card.Img variant="top" src={img} />
                 <Card.Body>
-                    <Card.Title>My Title</Card.Title>
-                    <Card.Text>This is my lorem text. That is given to me to . Gibrish text . That have not any minning</Card.Text>
+                    <div className="d-flex justify-content-between">
+                        <h6 className='mb-0'>{name}</h6>
+                        <small className='text-muted p-0'>( Quantity: {quantity} )</small>
+                    </div>
+                    <small className='text-muted'>Supplier: {supplier}</small>
+                    <Card.Subtitle className='brand-text mt-1'>${price}</Card.Subtitle>
+                    <Card.Text>{description.slice(0, 80).concat('...')}</Card.Text>
                     <div className="text-end">
                         <button className='brand-btn-sm'>Update Stock</button>
                     </div>
--- src/Pages/Home/InventoryItems/InventoryItems.js
+++ src/Pages/Home/InventoryItems/InventoryItems.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
 import InventoryItem from '../InventoryItem/InventoryItem';
 
 const InventoryItems = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setItems(data));
+    },[setItems]);
+
     return (
         <div className='mt-5'>
             <p className='text-center fs-4'>Inventory Items</p>
             <Row xs={1} sm={1} md={2} lg={3} className="g-5 me-auto ms-auto">
-                <InventoryItem></InventoryItem>
-                <InventoryItem></InventoryItem>
-                <InventoryItem></InventoryItem>
-                <InventoryItem></InventoryItem>
-                <InventoryItem></InventoryItem>
-                <InventoryItem></InventoryItem>
+                {
+                    items.map(item => <InventoryItem
+                        key={item._id}
+                        item={item}
+                    ></InventoryItem>)
+                }
             </Row>
             <div className="text-center mt-3">
                 <button className='brand-btn'>Manage Inventories</button>
