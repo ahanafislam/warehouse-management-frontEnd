dac1b10639b5d0ca8c3d9e3f79ea66ba30587a72
ahanafislam <ahanafislam@gmail.com>
2022-05-09 12:48:34 +0600

    Add firebase signup

--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile} from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Registration = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    
+    let errorElement;
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const handleRegister = async event => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+    }
+
+    useEffect(() => {
+        user && navigate("/");
+    },[user, navigate]);
+
+    if(loading || updating) {
+        console.log("Lodding...");
+    }
+
+    if(error || updateError) {
+        errorElement = <p className='text-danger'>{error?.message} {updateError?.message}</p>
+    }
+
     return (
         <Container>
             <div className='d-flex justify-content-center align-items-center me-auto ms-auto'>
                 <div className="form-shadow p-5">
                     <h2 className='brand-text text-center'>Sign Up</h2>
-                    <Form>
+                    {errorElement}
+                    <Form onSubmit={handleRegister}>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Name" required/>
+                            <Form.Control ref={nameRef} type="text" placeholder="Name" required/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" required/>
+                            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" required/>
+                            <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                         </Form.Group>
                         <button className='brand-btn' type="submit">
                             Sign Up
