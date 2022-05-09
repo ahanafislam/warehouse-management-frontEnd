22448ba2236313a4615ad5d387f668787c90fcac
ahanafislam <ahanafislam@gmail.com>
2022-05-09 16:31:11 +0600

    Degian Inventory detail page

--- src/Pages/Home/InventoryItems/InventoryItems.js
+++ src/Pages/Home/InventoryItems/InventoryItems.js
@@ -6,7 +6,7 @@ const InventoryItems = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/inventory')
             .then(res => res.json())
             .then(data => setItems(data));
     },[setItems]);
--- src/Pages/Inventory/InventoryDetails/InventoryDetails.js
+++ src/Pages/Inventory/InventoryDetails/InventoryDetails.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const InventoryDetails = () => {
     const {item_id} = useParams();
+    const [item, setItem] = useState({});
+    
+    const url = `http://localhost:5000/inventory/${item_id}`;
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setItem(data))
+    },[url]);
 
     return (
         <Container>
-            <h1>This is Inventory Details Page</h1>
-            <h2>Items id : {item_id}</h2>
+            <Card className="border-0">
+                <div className="row g-3">
+                    <div className="col-md-4">
+                        <img src={item.img} className="img-fluid rounded-start" alt="itemPicture"/>
+                    </div>
+                    <div className="col-md-8">
+                    <Card.Body>
+                        <h5>{item.name}</h5>
+                        <h6>Supplier : {item.supplier}</h6>
+                        <h6>Product Id : {item._id}</h6>
+                        <p>{item.description}</p>
+                        <br></br>
+                        <h6>Total Stock: {item.quantity}</h6>
+                        <hr className='brand-text mt-2'/>
+                        <button className='brand-btn'>Delivered</button>
+                    </Card.Body>
+                    </div>
+                </div>
+            </Card>
         </Container>
     );
 };
